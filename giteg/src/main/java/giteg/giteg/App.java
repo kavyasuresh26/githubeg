a1f47c15d6f082839b45460afa368780524c0973
kavyasuresh26 <kavyasuresh2630@gmail.com>
2024-10-19 15:35:29 +0530

    final try

---
+++ giteg/src/main/java/giteg/giteg/App.java
@@ -0,0 +1,10 @@
+package giteg.giteg;
+
+/**
+ * Hello world!
+ */
+public class App {
+    public static void main(String[] args) {
+        System.out.println("Hello World!");
+    }
+}
---
+++ giteg/src/test/java/giteg/giteg/test.java
@@ -0,0 +1,10 @@
+package giteg.giteg;
+
+public class test {
+	
+	public static void main(String[] args) {
+	
+	System.out.println("test");
+	}
+
+}
